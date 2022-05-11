f47305fcce06b81cab0e6d29e20dac7f71211b13
Ben Ashby <benashby@Bens-MacBook-Pro-3.local>
2022-05-11 14:33:04 +1000

    Update:

--- infra/backend.tf
+++ infra/backend.tf
@@ -5,10 +5,11 @@ terraform {
 }
 
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "=4.4.0"
     }
   }
 }

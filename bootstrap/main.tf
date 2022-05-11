55a95c1286b83a761fa75faa3b49565452a461d3
Ben Ashby <benashby@Bens-MacBook-Pro-3.local>
2022-05-11 10:32:00 +1000

    Update

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,7 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region  = "ap-southeast-2"
 }
-
 
 resource "random_string" "tfstatename" {
   length = 6
--- infra/backend.tf
+++ infra/backend.tf
@@ -3,3 +3,12 @@ terraform {
     region = "ap-southeast-2"
   }
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
